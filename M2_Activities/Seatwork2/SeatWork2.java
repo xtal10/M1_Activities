f93d6c4d05246074310f625ea7decca4a4f4d908
Christian Dave S. Cania <gstcscania@robinsonsbank.com.ph>
2025-12-05 11:09:20 +0800

    Seatwork2

---
+++ M2_Activities/Seatwork2/SeatWork2.java
@@ -0,0 +1,15 @@
+package M2_Activity02;
+
+public class SeatWork2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WhiteBoards wb= new WhiteBoards();
+		int sum=wb.addnumber(5,3);
+		
+		System.out.println("The total is : "+String.valueOf(sum));
+
+	}
+
+}
---
+++ M2_Activities/Seatwork2/WhiteBoards.java
@@ -0,0 +1,16 @@
+package M2_Activity02;
+
+public class WhiteBoards {
+	
+	
+	//Method computation
+	int addnumber(int addend1,int addend2) {
+		
+		int sum = addend1+addend2;
+		
+		
+		return sum;
+		
+	}
+
+}
