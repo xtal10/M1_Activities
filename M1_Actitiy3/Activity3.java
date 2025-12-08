e3ebe417db3d5e17701e44e68219ebc939b67394
Christian Dave S. Cania <gstcscania@robinsonsbank.com.ph>
2025-12-08 12:41:07 +0800

    Repushing this activity

--- M1_Actitiy3/Activity3.java
+++ M1_Actitiy3/Activity3.java
@@ -1,7 +1,19 @@
-package Activity.M1_Actitiy3;
+package Activity.BPI_Java_Activities.M1_Actitiy3;
 import java.util.Scanner;
 
 public class Activity3 {
+	
+	
+	static void myMethod(int num1, int num2) {
+		
+		int sum=num1+num2;
+		int dif=num1-num2;
+		int prod= num1*num2;
+		System.out.println("Sum : "+sum);
+		System.out.println("Difference : "+dif);
+		System.out.println("Product : "+prod);
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,12 +24,9 @@ public class Activity3 {
 		System.out.print("Enter second integers:");
 		int num2 = input.nextInt();
 		
-		int sum=num1+num2;
-		int dif=num1-num2;
-		int prod= num1*num2;
-		System.out.println("Sum : "+sum);
-		System.out.println("Difference : "+dif);
-		System.out.println("Product : "+prod);
+		myMethod(num1,num2);
+		
+	
 		
 	}
 
